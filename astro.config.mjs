af5cc24e0fe005a4a02b6fb8267e6ff031d6bbf9
Wilson Gomez <developer1@presik.com>
2026-05-02 16:47:34 -0500

    feat: update branding, add sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,14 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
-
 import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://cortesydoblecesmg.com',
   vite: {
     plugins: [tailwindcss()]
   },
 
-  integrations: [react()]
+  integrations: [react(), sitemap()]
 });
